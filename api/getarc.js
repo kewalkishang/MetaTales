854fa8669cc36199f8825ee5fbac95d9b130bee9
Kewal Kishan Gokuldas <Kewalkishang@gmail.com>
2024-07-07 12:09:29 -0700

    fixing list issue with arc

--- api/getarc.js
+++ api/getarc.js
@@ -35,10 +35,14 @@ export const getAllUserArc = async (data) => {
             return { success: false, message: "No items found in the response" };
         }
 
+        
+      const dataWithUser = parsedData.items
+      .filter(item => item.username && item.username.S === username)  
+
       //  console.log('Parsed ARC data:', parsedData.items); 
         console.log('Parsed ARC data:', parsedData.items[0].coverurl.S); 
         const bucketBaseUrl = process.env.EXPO_PUBLIC_S3_ENDPOINT;
-        const dataWithFullImageUrls = parsedData.items.map(item => ({
+        const dataWithFullImageUrls = dataWithUser.map(item => ({
             ...item,
             comicurl: item.comicid.L.map(comic => {
                 // Accessing the structure correctly based on your JSON structure
@@ -58,66 +62,7 @@ export const getAllUserArc = async (data) => {
 };
 
 
-export const getAllComics = async (data) => {
-  console.log("Getting all comics");
-  const username = data.username;
 
-  const currtime = "Time";
-
-  const updatedData = {
-      username: username,
-      time : currtime,
-  };
-
-  const config = {
-      headers: {
-          'Content-Type': 'application/json',
-          "Access-Control-Allow-Origin": '*',
-          // 'Authorization': `Bearer ${token}`,
-      }
-  };
-
-  // Attempt to add the item to the DynamoDB table
-  try {
-      const response = await axios.get(process.env.EXPO_PUBLIC_COMIC_API_ENDPOINT, updatedData);
-      console.log('Response :', response.data);
-      const parsedData = JSON.parse(response.data.body);
-
-      console.log('Parsed response data:', parsedData);  // Log the parsed data for debugging
-
-      if (!parsedData.items) {
-          console.error('No items array found:', parsedData);
-          return { success: false, message: "No items found in the response" };
-      }
-
-
-      const imageURLs = parsedData.items
-      .filter(item => item.username && item.username.S === username)  // Check if the username matches
-      .map(item => item.imageURL.S);  // Extract imageURLs
-
-
-//         // Extracting messages from all items
-//  //const messages = parsedData.items[0].data.M.choices.L[0].M.message.M.content.S;
-//  const messages = extractAllMessages(parsedData);
-
-
-      const bucketBaseUrl = process.env.EXPO_PUBLIC_S3_ENDPOINT;
-
-const fullImageUrls = imageURLs.map(filename => {
-  // Encode each filename to handle special characters properly
-  const encodedFilename = encodeURIComponent(filename);
-  // Concatenate the base URL with the encoded filename
-  return `${bucketBaseUrl}${encodedFilename}`;
-});
-
-    console.log('Image URLs for', username, ':', fullImageUrls , " : " + imageURLs);
-      return { success: true, message: "Stories fetched successfully!", img: fullImageUrls , urls : imageURLs};;
-  } catch (error) {
-      console.error('Error:', error.message);
-      return { success: false, message: error.message };
-  }
-
-};
 
 
 const isFromToday = (isoDate) => {
